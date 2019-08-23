27580182d4cee745ab756709875e0ca16e99a07d
antares511 <37109848+antares511@users.noreply.github.com>
2019-08-23 18:25:04 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -2,14 +2,13 @@ def isVowel(ch):
     return (ch == 'a' or ch == 'e' or
             ch == 'i' or ch == 'o' or
             ch == 'u') 
-   
 #check if isVowel
   
 def isGoodWord(inStr):
     res = inStr.split() #splits the string using any delimiter. default is space
     
     outStr = []
-    for i in range(0,len(res)-1):
+    for i in range(0,len(res)):
         if isVowel(res[i][0].lower()) == True:
             if isVowel(res[i][len(res[i]) - 1].lower()) == False:
                 outStr.append(res[i])
@@ -18,6 +17,4 @@ def isGoodWord(inStr):
         
 # Main Code 
 strx = str(input("Input: "))
-print(isGoodWord(strx)) 
-  
-
+isGoodWord(strx)
